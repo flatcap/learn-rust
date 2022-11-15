6dc5da9f8cb085aad2f9822562aac1cf83e03bb2
Richard Russon <rich@flatcap.org>
2022-11-15 17:00:52 +0000

    multi-param

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 #![allow(unused)]
 
 fn main() {
-    println!("Hello, world!");
-
-    another_function();
+    another_function(5);
+    print_labeled_measurement(5, 'h');
 }
 
-fn another_function() {
-    println!("Another function.");
+fn another_function(x: i32) {
+    println!("The value of x is: {x}");
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
 }
