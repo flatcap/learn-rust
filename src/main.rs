e1ec72964d8c56d4bf5d422796771cd2926314fb
Richard Russon <rich@flatcap.org>
2022-11-15 17:05:06 +0000

    expression

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,10 @@
 #![allow(unused)]
 
 fn main() {
-    another_function(5);
-    print_labeled_measurement(5, 'h');
-}
+    let y = {
+        let x = 3;
+        x + 1  // note: no semicolon
+    };
 
-fn another_function(x: i32) {
-    println!("The value of x is: {x}");
-}
-
-fn print_labeled_measurement(value: i32, unit_label: char) {
-    println!("The measurement is: {value}{unit_label}");
+    println!("The value of y is: {y}");
 }
