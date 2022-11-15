940e843ada99b2adcc88cdd9ff3e2b3b2aa4ab50
Richard Russon <rich@flatcap.org>
2022-11-15 17:08:49 +0000

    retval 2

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 #![allow(unused)]
 
-fn five() -> i32 {
-    5
-}
-
 fn main() {
-    let x = five();
+    let x = plus_one(5);
 
     println!("The value of x is: {x}");
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
