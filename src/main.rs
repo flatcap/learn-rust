7a1fb33f7534879947baf42ce1394b5cc17402e2
Richard Russon <rich@flatcap.org>
2022-11-15 16:56:55 +0000

    function

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,11 @@
 #![allow(unused)]
-#![allow(unconditional_panic)]
 
 fn main() {
-    let a = [1, 2, 3, 4, 5];
+    println!("Hello, world!");
 
-    println!("The value of a[3] is: {}", a[3]);
+    another_function();
+}
 
-    let b: [i32; 5] = [1, 2, 3, 4, 5];
-
-    println!("The value of b[3] is: {}", b[3]);
-
-    let c: [i32; 5] = [1; 5]; // equivalent to [1, 1, 1, 1, 1]
-
-    println!("The value of c[3] is: {}", c[3]);
-
-    let _ = c[5];
+fn another_function() {
+    println!("Another function.");
 }
