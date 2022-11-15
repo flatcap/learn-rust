f1ed41d5ecbe0dde36e6fb79c86854a11712800f
Richard Russon <rich@flatcap.org>
2022-11-15 16:37:13 +0000

    tuple

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,8 @@
 fn main() {
-    let x = 5;
+    let tup = (500, 6.4, 1);
 
-    let x = x + 1;
+    let (_, y, _) = tup;
 
-    {
-        let x = x * 2;
-        println!("The value of x in the inner scope is: {x}");
-    }
-
-    println!("The value of x is: {x}");
+    println!("The value of y is: {y}");
 }
+
