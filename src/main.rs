0cc59832fa2eaafa3ee793a4b1ff169b5165ab6c
Richard Russon <rich@flatcap.org>
2022-11-15 16:39:15 +0000

    destructure

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,12 @@ fn main() {
     let (_, y, _) = tup;
 
     println!("The value of y is: {y}");
-}
 
+    let x: (i32, f64, u8) = (500, 6.4, 1);
+
+    let _five_hundred = x.0;
+
+    let _six_point_four = x.1;
+
+    let _one = x.2;
+}
