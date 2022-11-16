8b4123a141017453120eb062deee4269a244aa1b
Richard Russon <rich@flatcap.org>
2022-11-16 11:55:04 +0000

    for array

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
 fn main() {
     let a = [10, 20, 30, 40, 50];
-    let mut index = 0;
 
-    while index < 5 {
-        println!("the value is: {}", a[index]);
-
-        index += 1;
+    for element in a {
+        println!("the value is: {element}");
     }
 }
