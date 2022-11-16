8490368f21ffd4b4b185a513d4176ff80cad1755
Richard Russon <rich@flatcap.org>
2022-11-16 11:54:10 +0000

    while 2

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 fn main() {
-    let mut number = 3;
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
 
-    while number != 0 {
-        println!("{number}!");
+    while index < 5 {
+        println!("the value is: {}", a[index]);
 
-        number -= 1;
+        index += 1;
     }
-
-    println!("LIFTOFF!!!");
 }
