f53b8acdef5c417473df99b9b8d85fcfe0aa9663
Richard Russon <rich@flatcap.org>
2022-11-16 11:53:50 +0000

    while

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,11 @@
 fn main() {
-    let mut count = 0;
-    'counting_up: loop {
-        println!("count = {count}");
-        let mut remaining = 10;
+    let mut number = 3;
 
-        loop {
-            println!("remaining = {remaining}");
-            if remaining == 9 {
-                break;
-            }
-            if count == 2 {
-                break 'counting_up;
-            }
-            remaining -= 1;
-        }
+    while number != 0 {
+        println!("{number}!");
 
-        count += 1;
+        number -= 1;
     }
-    println!("End count = {count}");
+
+    println!("LIFTOFF!!!");
 }
