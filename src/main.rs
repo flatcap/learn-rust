79fd6be741e622a4980f3402b78e01678494ed65
Richard Russon <rich@flatcap.org>
2022-11-16 13:32:00 +0000

    fibonacci numbers

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+use std::io;
+use std::io::Write;
+
+// Generate the n'th Fibonacci number
+fn fibonacci(n: u32) -> u32 {
+    if n == 0 {
+        0
+    } else if n == 1 {
+        1
+    } else {
+        fibonacci(n - 2) + fibonacci(n - 1)
+    }
+}
+
+fn main() {
+    loop {
+        print!("Enter n: ");
+        io::stdout().flush().unwrap();
+
+        let mut n = String::new();
+        io::stdin().read_line(&mut n).expect("Failed to read line");
+
+        let n = n.trim();
+        if n == "" {
+            break;
+        }
+
+        let n: u32 = match n.parse() {
+            Ok(n) => n,
+            Err(_) => {
+                println!("Invalid number");
+                break;
+            }
+        };
+
+        let fib = fibonacci(n);
+
+        println!("Fibonacci number {n} is {fib}");
+    }
+}
