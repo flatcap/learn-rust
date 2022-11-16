358a0b1c7ccae6bf5c78e696d10ebf11f6aeeb23
Richard Russon <rich@flatcap.org>
2022-11-16 11:56:06 +0000

    for rev

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 fn main() {
-    let a = [10, 20, 30, 40, 50];
-
-    for element in a {
-        println!("the value is: {element}");
+    for number in (1..4).rev() {
+        println!("{number}!");
     }
+    println!("LIFTOFF!!!");
 }
