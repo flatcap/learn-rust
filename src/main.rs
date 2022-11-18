adb07bcaca93d596144e132be6db0524d976dca1
Richard Russon <rich@flatcap.org>
2022-11-18 16:22:45 +0000

    expect to panic

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,6 @@ use std::fs::File;
 use std::io::ErrorKind;
 
 fn main() {
-    let greeting_file = File::open("hello.txt").unwrap();
+    let greeting_file = File::open("hello.txt")
+        .expect("hello.txt should be included in this project");
 }
