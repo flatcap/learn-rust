5d20d6299e7f21537704aa0ee7c5d78a59b75a3f
Richard Russon <rich@flatcap.org>
2022-11-18 16:39:41 +0000

    read directly into string

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,13 @@
 #![allow(unused)]
 
+use std::fs;
 use std::fs::File;
 use std::io;
 use std::io::ErrorKind;
 use std::io::Read;
 
 fn read_username_from_file() -> Result<String, io::Error> {
-    let mut username = String::new();
-
-    File::open("hello.txt")?.read_to_string(&mut username)?;
-
-    Ok(username)
+    fs::read_to_string("hello.txt")
 }
 
 fn main() {
