0626876697489e18d407c0e67d4598fe7b59674d
Richard Russon <rich@flatcap.org>
2022-11-18 16:28:05 +0000

    propagate error

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,27 @@
 #![allow(unused)]
 
 use std::fs::File;
+use std::io;
 use std::io::ErrorKind;
+use std::io::Read;
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let username_file_result = File::open("hello.txt");
+
+    let mut username_file = match username_file_result {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut username = String::new();
+
+    match username_file.read_to_string(&mut username) {
+        Ok(_) => Ok(username),
+        Err(e) => Err(e),
+    }
+}
 
 fn main() {
-    let greeting_file = File::open("hello.txt")
-        .expect("hello.txt should be included in this project");
+    let greeting_file =
+        File::open("hello.txt").expect("hello.txt should be included in this project");
 }
