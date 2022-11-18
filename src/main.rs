88abddb373a7db1638f07786efce60b0bd26f50a
Richard Russon <rich@flatcap.org>
2022-11-18 16:22:07 +0000

    unwrap to panic

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,5 @@ use std::fs::File;
 use std::io::ErrorKind;
 
 fn main() {
-    let greeting_file = File::open("hello.txt").unwrap_or_else(|error| {
-        if error.kind() == ErrorKind::NotFound {
-            File::create("hello.txt").unwrap_or_else(|error| {
-                panic!("Problem creating the file: {:?}", error);
-            })
-        } else {
-            panic!("Problem opening the file: {:?}", error);
-        }
-    });
+    let greeting_file = File::open("hello.txt").unwrap();
 }
