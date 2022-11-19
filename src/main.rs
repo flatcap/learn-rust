a62635302f6135a6055d815471bd659c8cd0bf21
Richard Russon <rich@flatcap.org>
2022-11-19 02:18:36 +0000

    struct fn using diff types

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,24 @@
 #![allow(unused)]
 
-enum Result<T, E> {
-    Ok(T),
-    Err(E),
+struct Point<X1, Y1> {
+    x: X1,
+    y: Y1,
 }
 
-struct Point<T, U> {
-    x: T,
-    y: U,
-}
-
-impl<T, U> Point<T, U> {
-    fn x(&self) -> &T {
-        &self.x
-    }
-}
-
-impl Point<f32, f32> {
-    fn distance_from_origin(&self) -> f32 {
-        (self.x.powi(2) + self.y.powi(2)).sqrt()
+impl<X1, Y1> Point<X1, Y1> {
+    fn mixup<X2, Y2>(self, other: Point<X2, Y2>) -> Point<X1, Y2> {
+        Point {
+            x: self.x,
+            y: other.y,
+        }
     }
 }
 
 fn main() {
-    let both_integer = Point { x: 5, y: 10 };
-    let both_float = Point { x: 1.0, y: 4.0 };
-    let integer_and_float = Point { x: 5, y: 4.0 };
+    let p1 = Point { x: 5, y: 10.4 };
+    let p2 = Point { x: "Hello", y: 'c' };
 
-    let p = Point { x: 5, y: 10 };
-    println!("p.x = {}", p.x());
+    let p3 = p1.mixup(p2);
+
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
 }
