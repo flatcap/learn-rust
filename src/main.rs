91bd1d4147c884c0cfb8c4f256eed6cbf6892b62
Richard Russon <rich@flatcap.org>
2022-11-19 19:57:39 +0000

    read file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::env;
+use std::fs;
 
 fn main() {
+    // --snip--
     let args: Vec<String> = env::args().collect();
 
     let query = &args[1];
@@ -8,4 +10,8 @@ fn main() {
 
     println!("Searching for {}", query);
     println!("In file {}", file_path);
+
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
 }
