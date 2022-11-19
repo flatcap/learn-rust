7806d59f3ebd904bc9c07949136ffc3f7167c728
Richard Russon <rich@flatcap.org>
2022-11-19 02:05:55 +0000

    multi-type generic struct members

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 #![allow(unused)]
 
-struct Point<T> {
+struct Point<T, U> {
     x: T,
-    y: T,
+    y: U,
 }
 
 fn main() {
-    let integer = Point { x: 5, y: 10 };
-    let float = Point { x: 1.0, y: 4.0 };
+    let both_integer = Point { x: 5, y: 10 };
+    let both_float = Point { x: 1.0, y: 4.0 };
+    let integer_and_float = Point { x: 5, y: 4.0 };
 }
