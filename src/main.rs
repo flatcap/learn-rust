9c52f5aa3c2f3ec3d2e954727566c06c8db8405a
Richard Russon <rich@flatcap.org>
2022-11-19 01:49:51 +0000

    largest multi-types

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,16 @@
-fn largest(list: &[i32]) -> &i32 {
+fn largest_i32(list: &[i32]) -> &i32 {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+fn largest_char(list: &[char]) -> &char {
     let mut largest = &list[0];
 
     for item in list {
@@ -13,13 +25,13 @@ fn largest(list: &[i32]) -> &i32 {
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
 
-    let result = largest(&number_list);
+    let result = largest_i32(&number_list);
     println!("The largest number is {}", result);
     assert_eq!(*result, 100);
 
-    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+    let char_list = vec!['y', 'm', 'a', 'q'];
 
-    let result = largest(&number_list);
-    println!("The largest number is {}", result);
-    assert_eq!(*result, 6000);
+    let result = largest_char(&char_list);
+    println!("The largest char is {}", result);
+    assert_eq!(*result, 'y');
 }
