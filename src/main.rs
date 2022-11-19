454f7ada067588966054c4eec85c79670408c7b9
Richard Russon <rich@flatcap.org>
2022-11-19 02:11:19 +0000

    generic fn on struct

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,17 @@ struct Point<T, U> {
     y: U,
 }
 
+impl<T, U> Point<T, U> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
 fn main() {
     let both_integer = Point { x: 5, y: 10 };
     let both_float = Point { x: 1.0, y: 4.0 };
     let integer_and_float = Point { x: 5, y: 4.0 };
+
+    let p = Point { x: 5, y: 10 };
+    println!("p.x = {}", p.x());
 }
