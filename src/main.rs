04637adb278987622a06061e142e106f975fc8de
Richard Russon <rich@flatcap.org>
2022-11-19 02:28:43 +0000

    demo

--- src/main.rs
+++ src/main.rs
@@ -29,5 +29,12 @@ impl Summary for Tweet {
 }
 
 fn main() {
-    println!("Hello, world!");
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summarize());
 }
