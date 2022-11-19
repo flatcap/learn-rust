7cd110e11ff98879b740f009d5e9f904fbc29fc3
Richard Russon <rich@flatcap.org>
2022-11-19 02:14:09 +0000

    fn limited to specific types

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,12 @@ impl<T, U> Point<T, U> {
     }
 }
 
+impl Point<f32, f32> {
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+
 fn main() {
     let both_integer = Point { x: 5, y: 10 };
     let both_float = Point { x: 1.0, y: 4.0 };
