0a2df7aedebbd652dfff472de179cca78adca16b
Richard Russon <rich@flatcap.org>
2022-11-19 15:40:04 +0000

    add lifetime to struct member

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+#![allow(unused)]
+
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
 fn main() {
     let string1 = String::from("long string is long");
 
@@ -6,6 +12,12 @@ fn main() {
         let result = longest(string1.as_str(), string2.as_str());
         println!("The longest string is {}", result);
     }
+
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
 }
 
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
