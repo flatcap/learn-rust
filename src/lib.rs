ea690f79eb8c6b8206ff8a78cc8213c5af5d5414
Richard Russon <rich@flatcap.org>
2022-11-19 17:55:44 +0000

    test should_panic

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,19 @@
 #![allow(unused)]
 
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
+
+        Guess { value }
+    }
+}
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
@@ -32,5 +46,11 @@ mod tests {
             "Greeting did not contain name, value was `{}`",
             result
         );
+    }
+
+    #[test]
+    #[should_panic]
+    fn greater_than_100() {
+        Guess::new(200);
     }
 }
