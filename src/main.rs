a83928060280d50b843ee800e992a20cf9928a8c
Richard Russon <rich@flatcap.org>
2022-11-19 19:56:40 +0000

    dump args

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,10 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
+
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", file_path);
 }
