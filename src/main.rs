95e8d03b762b09c0e81b8e9b0a4dfad46165295b
Richard Russon <rich@flatcap.org>
2022-11-19 01:55:10 +0000

    generic largest

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,4 @@
-fn largest_i32(list: &[i32]) -> &i32 {
-    let mut largest = &list[0];
-
-    for item in list {
-        if item > largest {
-            largest = item;
-        }
-    }
-
-    largest
-}
-
-fn largest_char(list: &[char]) -> &char {
+fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
     let mut largest = &list[0];
 
     for item in list {
@@ -25,13 +13,11 @@ fn largest_char(list: &[char]) -> &char {
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
 
-    let result = largest_i32(&number_list);
+    let result = largest(&number_list);
     println!("The largest number is {}", result);
-    assert_eq!(*result, 100);
 
     let char_list = vec!['y', 'm', 'a', 'q'];
 
-    let result = largest_char(&char_list);
+    let result = largest(&char_list);
     println!("The largest char is {}", result);
-    assert_eq!(*result, 'y');
 }
