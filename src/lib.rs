b75f5050ac77370bf8f4bb010cedf569c17a357c
Richard Russon <rich@flatcap.org>
2022-11-19 18:07:03 +0000

    specific test panic messages

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,16 @@ pub struct Guess {
 
 impl Guess {
     pub fn new(value: i32) -> Guess {
-        if value < 1 || value > 100 {
-            panic!("Guess value must be between 1 and 100, got {}.", value);
+        if value < 1 {
+            panic!(
+                "Guess value must be greater than or equal to 1, got {}.",
+                value
+            );
+        } else if value > 100 {
+            panic!(
+                "Guess value must be less than or equal to 100, got {}.",
+                value
+            );
         }
 
         Guess { value }
