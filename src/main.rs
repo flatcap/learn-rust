e6985e5b5188fd370a368f9c87917a578228a0b9
Richard Russon <rich@flatcap.org>
2022-11-24 18:20:13 +0000

    split out parsing

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,9 @@ use std::env;
 use std::fs;
 
 fn main() {
-    // --snip--
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let (query, file_path) = parse_config(&args);
 
     println!("Searching for {}", query);
     println!("In file {}", file_path);
@@ -14,4 +12,11 @@ fn main() {
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
 
     println!("With text:\n{contents}");
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let file_path = &args[2];
+
+    (query, file_path)
 }
