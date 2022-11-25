f7344004cc6aa780947b12050979e68d8cbf6813
Richard Russon <rich@flatcap.org>
2022-11-25 10:10:07 +0000

    tdd: working test search case insensitive

--- src/lib.rs
+++ src/lib.rs
@@ -42,7 +42,16 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    vec![]
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
 }
 
 #[cfg(test)]
