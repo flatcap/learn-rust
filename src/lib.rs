d95b84fd76e88e9eed0405898cbc2325cc029261
Richard Russon <rich@flatcap.org>
2022-11-25 00:21:18 +0000

    tdd: test succeeds

--- src/lib.rs
+++ src/lib.rs
@@ -26,7 +26,15 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    vec![]
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
 }
 
 #[cfg(test)]
