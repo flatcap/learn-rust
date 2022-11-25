f2d3e5dd36037bba0044b8a9186a102c6f69c1e2
Richard Russon <rich@flatcap.org>
2022-11-25 00:07:27 +0000

    drop debugging

--- src/lib.rs
+++ src/lib.rs
@@ -22,7 +22,5 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    println!("With text:\n{contents}");
-
     Ok(())
 }
