32e437fcdbb5550fdae0cd68867436a749c251c5
Richard Russon <rich@flatcap.org>
2022-11-26 00:36:03 +0000

    map()

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,18 @@ fn iterator_sum() {
     println!("total = {total}");
 }
 
+fn iterator_map() {
+    let v1 = vec![1, 2, 3];
+
+    let v1_iter = v1.iter().map(|x| x + 7);
+
+    for val in v1_iter {
+        println!("Got: {}", val);
+    }
+}
+
 fn main() {
     iterator_simple();
     iterator_sum();
+    iterator_map();
 }
