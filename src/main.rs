415da987085e7a71dd84ca8b18a692cd009ad940
Richard Russon <rich@flatcap.org>
2022-11-26 00:31:09 +0000

    sum

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-fn main() {
+fn iterator_simple() {
     let v1 = vec![1, 2, 3];
 
     let v1_iter = v1.iter();
@@ -6,4 +6,21 @@ fn main() {
     for val in v1_iter {
         println!("Got: {}", val);
     }
+}
+
+fn iterator_sum() {
+    let v1 = vec![1, 2, 3];
+
+    let v1_iter = v1.iter();
+
+    // sum() consumes the iterator
+    let total: i32 = v1_iter.sum();
+
+    assert_eq!(total, 6);
+    println!("total = {total}");
+}
+
+fn main() {
+    iterator_simple();
+    iterator_sum();
 }
