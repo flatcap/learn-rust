42272df08dd3a62554c88305851af70166df3408
Richard Russon <rich@flatcap.org>
2022-11-27 18:17:07 +0000

    list type

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,22 @@
-fn main() {
+#![allow(unused)]
+
+use crate::List::{Cons, Nil};
+
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
+
+fn test_box() {
     let b = Box::new(5);
     println!("b = {}", b);
+}
+
+fn test_list() {
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+}
+
+fn main() {
+    test_box();
+    test_list();
 }
