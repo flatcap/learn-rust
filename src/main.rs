53448483eb2c76bc9e385c271f975e3d7335e35d
Richard Russon <rich@flatcap.org>
2022-11-27 18:18:15 +0000

    deref box

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,16 @@ fn test_list() {
     let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
 }
 
+fn test_deref() {
+    let x = 5;
+    let y = Box::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+}
+
 fn main() {
     test_box();
     test_list();
+    test_deref();
 }
