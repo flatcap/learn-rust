82e7cee46df0a181d16c03635f4ef8e58f41707f
Richard Russon <rich@flatcap.org>
2022-11-27 19:10:46 +0000

    upgrade list to use Rc

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,22 @@
 
 use crate::List::{Cons, Nil};
 use std::ops::Deref;
+use std::rc::Rc;
 
 enum List {
-    Cons(i32, Box<List>),
+    Cons(i32, Rc<List>),
     Nil,
+}
+
+fn test_list() {
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    let b = Cons(3, Rc::clone(&a));
+    let c = Cons(4, Rc::clone(&a));
 }
 
 fn test_box() {
     let b = Box::new(5);
     println!("b = {}", b);
-}
-
-fn test_list() {
-    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
 }
 
 fn test_deref() {
