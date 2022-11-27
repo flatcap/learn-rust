f147e207f15a1f50d4a458b7366c6cb99ffb5045
Richard Russon <rich@flatcap.org>
2022-11-27 18:21:11 +0000

    mybox with deref trait

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![allow(unused)]
 
 use crate::List::{Cons, Nil};
+use std::ops::Deref;
 
 enum List {
     Cons(i32, Box<List>),
@@ -24,8 +25,33 @@ fn test_deref() {
     assert_eq!(5, *y);
 }
 
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+fn test_mybox() {
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+}
+
 fn main() {
     test_box();
     test_list();
     test_deref();
+    test_mybox();
 }
