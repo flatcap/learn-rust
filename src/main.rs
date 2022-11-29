b7cc7c0c0d0e787f26fdd49c35003afafa96fc55
Richard Russon <rich@flatcap.org>
2022-11-29 00:35:35 +0000

    move data to thread

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+#![allow(unused)]
+
 use std::thread;
 use std::time::Duration;
 
-fn main() {
+fn thread_simple() {
     let handle = thread::spawn(|| {
         for i in 1..10 {
             println!("hi number {} from the spawned thread!", i);
@@ -15,4 +17,19 @@ fn main() {
     }
 
     handle.join().unwrap();
+}
+
+fn thread_move() {
+    let v = vec![1, 2, 3];
+
+    let handle = thread::spawn(move || {
+        println!("Here's a vector: {:?}", v);
+    });
+
+    handle.join().unwrap();
+}
+
+fn main() {
+    // thread_simple();
+    thread_move();
 }
