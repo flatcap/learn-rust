61bdea1f94b41296dff8fb7a7a96f7ac0f370297
Richard Russon <rich@flatcap.org>
2022-12-01 23:50:59 +0000

    associated trait

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+#![allow(unused)]
+
+pub trait Iterator {
+    // associated type
+    type Item;
+
+    fn next(&mut self) -> Option<Self::Item>;
+}
+
+struct Counter {
+    count: u32,
+}
+
+impl Counter {
+    fn new() -> Counter {
+        Counter { count: 0 }
+    }
+}
+
+impl Iterator for Counter {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.count < 5 {
+            self.count += 1;
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
+
+fn main(){}
