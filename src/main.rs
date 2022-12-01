54df47ef0547091311ec3dcd59ff2fa325e61f1a
Richard Russon <rich@flatcap.org>
2022-12-01 23:54:10 +0000

    ambiguous fn names

--- src/main.rs
+++ src/main.rs
@@ -67,6 +67,42 @@ impl Add<Meters> for Millimeters {
     }
 }
 
+trait Pilot {
+    fn fly(&self);
+}
+
+trait Wizard {
+    fn fly(&self);
+}
+
+struct Human;
+
+impl Pilot for Human {
+    fn fly(&self) {
+        println!("This is your captain speaking.");
+    }
+}
+
+impl Wizard for Human {
+    fn fly(&self) {
+        println!("Up!");
+    }
+}
+
+impl Human {
+    fn fly(&self) {
+        println!("*waving arms furiously*");
+    }
+}
+
+fn test_fly() {
+    let person = Human;
+    Pilot::fly(&person);
+    Wizard::fly(&person);
+    person.fly();
+}
+
 fn main() {
     test_point();
+    test_fly();
 }
