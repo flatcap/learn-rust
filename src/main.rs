fe95a21f975d5d055954324b13a760072f7b8f84
Richard Russon <rich@flatcap.org>
2022-12-01 23:52:00 +0000

    override trait add

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #![allow(unused)]
 
+use std::ops::Add;
+
 pub trait Iterator {
     // associated type
     type Item;
@@ -30,4 +32,30 @@ impl Iterator for Counter {
     }
 }
 
-fn main(){}
+#[derive(Debug, Copy, Clone, PartialEq)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl Add for Point {
+    type Output = Point;
+
+    fn add(self, other: Point) -> Point {
+        Point {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
+fn test_point() {
+    assert_eq!(
+        Point { x: 1, y: 0 } + Point { x: 2, y: 3 },
+        Point { x: 3, y: 3 }
+    );
+}
+
+fn main() {
+    test_point();
+}
