beb304f28d22e79f225e38b9242eabc99d6faa50
Richard Russon <rich@flatcap.org>
2022-12-02 17:33:33 +0000

    add 404 page

--- src/main.rs
+++ src/main.rs
@@ -23,12 +23,16 @@ fn handle_connection(mut stream: TcpStream) {
         let contents = fs::read_to_string("hello.html").unwrap();
         let length = contents.len();
 
-        let response = format!(
-            "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
-        );
+        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
         stream.write_all(response.as_bytes()).unwrap();
     } else {
-        // some other request
+        let status_line = "HTTP/1.1 404 NOT FOUND";
+        let contents = fs::read_to_string("404.html").unwrap();
+        let length = contents.len();
+
+        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+        stream.write_all(response.as_bytes()).unwrap();
     }
 }
