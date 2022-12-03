ef54264f9989ffb7c49e2a52de3eb9d2ed2aaf78
Richard Russon <rich@flatcap.org>
2022-12-03 00:01:20 +0000

    compiling ThreadPool

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use hello::ThreadPool;
 use std::{
     fs,
     io::{prelude::*, BufReader},
@@ -8,11 +9,12 @@ use std::{
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        thread::spawn(|| {
+        pool.execute(|| {
             handle_connection(stream);
         });
     }
